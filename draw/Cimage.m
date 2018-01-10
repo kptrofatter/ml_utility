c889b85c41d0b58d04dd79884f084866a64358af
K. Parker Trofatter <kptrofatter@gmail.com>
2018-01-10 13:02:04 -0500

    Fixed gradient issue with Cimage() around 1.0, gave Log() some documentation.

--- draw/Cimage.m
+++ draw/Cimage.m
@@ -6,70 +6,105 @@
 % Cimage() displays a complex valued 2D image.
 %
 % USAGE:
-%   [h] = Cimage(ah,x,y,z,p?,g?,b?,r?)
+%   [h] = Cimage(ah, x=1:,, y=1:n, z, type='linear', base=2, range=0.4, phase=0)
+%
 % INPUT:
-%   [1,1] axes    | ah | Axes handle. Setting ah = [] invokes the current axes.
-%   [1,m] double  | x  | X grid vector
-%   [1,n] double  | y  | Y grid vector
-%   [m,n] cdouble | z  | Complex data
-%   [1,1] double  | p  | [rad] Hue phase constant        [0.0,2.0*pi] (p? = 0.0)
-%   [1,1] double  | g  | Lightness gradient direction     {+1.0,-1.0} (g? = 1.0)
-%   [1,1] double  | b  | Lightness log base, 0 = no log          {R+} (b? = 2.0)
-%   [1,1] double  | r  | Lightness range                    [0.0,1.0] (r? = 0.5)
+%   [1,1] axes    | ah    | axes handle. [] gets current axes.
+%   [1,m] double  | x     | x grid vector
+%   [1,n] double  | y     | y grid vector
+%   [m,n] cdouble | z     | complex data
+%   [1,?] char    | type  | draw type {'flat', 'linear', 'log'}
+%   [1,1] double  | base  | lightness log base, 0.0=off [0.0, inf())
+%   [1,1] double  | range | lightness range size [0.0, 1.0]
+%   [1,1] double  | phase | [rad] hue phase offset [0.0, 2.0*pi]
+%
 % OUTPUT:
-%   [1,1] image   | h  | Image handle
-% NOTES:
-%   Either all optional arguments must be specified, or none at all.
+%   [1,1] ?       | h     | image or surf handle
 
-function [h] = Cimage(ah,x,y,z,p,g,b,r)
+function [h] = Cimage(ah, x, y, z, type, base, range, phase)
     
-    % Mode
-    if nargin()<5
-        p = 0.0; % [rad] Phase colormap offset
-        g = 1.0; % [#] Gradient direction
-        b = 2.0; % [#] Log base
-        r = 0.5; % [#] Intensity range
-    end
-    
-    % Compute HSL
-    H = mod(angle(z)+p,2.0*pi);
-    S = ones(size(H));
-    if b == 0.0
-        E = z;
-        L = (abs(E)-min(abs(E(:))))./(max(abs(E(:)))-min(abs(E(:))));
-    else
-        [~,~,E] = Split(Log(b,abs(z)));
-        E = (E - 0.5) * r + 0.5;
-        % Gradient
-        if g < 0.0
-            % Darker with more magnitude
-            L = b.^-abs(E);
-        else
-            % Lighter with more magnitude
-            L = 1.0 - b.^-abs(E);
-        end
-    end
-    
-    % Convert to RGB
-    [R,G,B] = Hsl2Rgb(H,S,L);
-    img(:,:,1) = R;
-    img(:,:,2) = G;
-    img(:,:,3) = B;
-    
-    % Axes
+    % defaults
     if isempty(ah)
         ah = gca();
     end
-    % Draw
+    if isempty(x) && isempty(y)
+        [nx, ny] = size(z);
+        x = 1 : nx;
+        y = 1 : ny;
+    end
+    if ~exist('type', 'var') || isempty(type)
+        type = 'linear';
+    end
+    if ~exist('base', 'var') || isempty(base)
+        base  = 2.0;
+    end
+    if ~exist('range', 'var') || isempty(range)
+        range = 0.4;
+    end
+    if ~exist('phase', 'var') || isempty(phase)
+        phase = 0.0;
+    end
+    
+    % compute hs_
+    H = mod(angle(z) + phase, 2.0 * pi);
+    S = ones(size(H));
+    
+    % compute __l
+    Z = abs(z);
+    Zlog = Log(base, Z);
+    if base == 0.0
+        
+        % linear gradient
+        Zmin = min(Z(:));
+        Zmax = max(Z(:));
+        L = (Z - Zmin) ./ (Zmax - Zmin);
+        
+    else
+        
+        % logarithmic gradient
+        L = Zlog - floor(Zlog);
+        
+    end
+        
+    % scale lightness = [0.0, 1.0] by range = [0.0, 1.0] and center
+    L = (L - 0.5) * range + 0.5;
+    
+    % convert hsl to rgb
+    [R, G, B] = Hsl2Rgb(H, S, L);
+    img(:, :, 1) = R;
+    img(:, :, 2) = G;
+    img(:, :, 3) = B;
+    
+    % matlab insanity (saved for the last second so other programming is sane)
+    img = permute(img, [2, 1, 3]);
+    
+    % draw begin
     hold(ah,'on');
-    h = image(x,y,flip(rot90(img)),'Parent',ah);
-    contour(ah,x,y,flip(rot90(abs(z))),0.95*[1,1],'Color','b','LineWidth',2);
-    contour(ah,x,y,flip(rot90(abs(z))),1.05*[1,1],'Color','r','LineWidth',2);
-    hold(ah,'off');
-    % Format
+    
+    % complex data
+    switch type
+    case 'flat'
+        % flat surface
+        h = image(x, y, img, 'Parent', ah);
+        
+    case 'linear'
+        % modular surface
+        h = surf(x, y, Z.', img, 'EdgeColor', 'none');
+        
+    case 'log'
+        % log of modular surface
+        h = surf(x, y, Zlog.', img, 'EdgeColor', 'none');
+        
+    end
+    
+    % draw end
+    hold(ah, 'off');
+    
+    % format
     ah.YDir = 'normal';
     
 end
+
 
 %==============================================================================%
 %                                                                              %
--- math/Log.m
+++ math/Log.m
@@ -1,4 +1,27 @@
-function [y] = Log(b,x)
-    y = log(x)./log(b);
+% %============================================================================%
+% % Duke University                                                            %
+% % K. P. Trofatter                                                            %
+% % kpt2@duke.edu                                                              %
+% %============================================================================%
+% Log() - computes a logarithm with a given base.
+%
+% USAGE:
+%   [y] = Log(base, x)
+%
+% INPUT:
+%   [1,1] double | base | logarithm base
+%   [?]   double | x    | input
+%
+% OUTPUT:
+%   [?]   double | y    | ouput
+
+function [y] = Log(base, x)
+    y = log(x) ./ log(base);
 end
 
+
+%==============================================================================%
+%                                                                              %
+%                                                                              %
+%                                                                              %
+%==============================================================================%
